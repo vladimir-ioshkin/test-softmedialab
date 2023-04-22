344749eb6ddf90e5e97216225b7980b4f73b6d64
Vladimir Ioshkin <bobbydigital92@gmail.com>
2023-04-22 16:17:45 +0300

    add tooltip

---
+++ src/components/InfoTooltip/InfoTooltip.tsx
@@ -0,0 +1,50 @@
+import { useRef, FC, useState } from 'react';
+import { Overlay } from 'react-bootstrap';
+import './styles.scss';
+
+const InfoTooltip: FC<{ text: string }> = ({ text }) => {
+    const [hover, setHover] = useState<boolean>(false);
+    const [show, setShow] = useState<boolean>(false);
+    const tooltipTriggerRef = useRef<HTMLDivElement>(null);
+
+    const tooltipClasses = 'info-tooltip-trigger';
+    const openedTooltipClasses = tooltipClasses.concat(' opened')
+
+    return (
+        <>
+            <div
+                className={show ? openedTooltipClasses : tooltipClasses}
+                ref={tooltipTriggerRef}
+                onClick={() => setShow((prev) => !prev)}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            />
+            <Overlay
+                target={tooltipTriggerRef.current}
+                placement='bottom-start'
+                show={hover || show}
+                offset={[11, 5]}
+            >
+                {/* eslint-disable @typescript-eslint/no-unused-vars */}
+                {({
+                      placement,
+                      arrowProps,
+                      show,
+                      popper,
+                      hasDoneInitialMeasure,
+                      ...props
+                }) => (
+                    <div
+                        {...props}
+                        className='info-tooltip'
+                    >
+                        {text}
+                    </div>
+                )}
+                {/* eslint-enable @typescript-eslint/no-unused-vars */}
+            </Overlay>
+        </>
+    );
+};
+
+export default InfoTooltip;
--- src/components/WageForm/Fields/PeriodRadio.tsx
+++ src/components/WageForm/Fields/PeriodRadio.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Field } from 'react-final-form';
 import CustomRadio from '../../CustomRadio/CustomRadio';
+import InfoTooltip from '../../InfoTooltip/InfoTooltip';
 import { WagePeriod } from '../../types';
 
 const PeriodRadio: FC = () => {
@@ -13,7 +14,10 @@ const PeriodRadio: FC = () => {
             </Field>
             <Field name='period' type='radio' value={WagePeriod.MinimalWage}>
                 {({ input }) => (
-                    <CustomRadio {...input} id={WagePeriod.MinimalWage} label='МРОТ' />
+                    <div className='d-flex gap-2'>
+                        <CustomRadio {...input} id={WagePeriod.MinimalWage} label='МРОТ' />
+                        <InfoTooltip text='МРОТ – минимальный размер оплаты труда. Разный для разных регионов.' />
+                    </div>
                 )}
             </Field>
             <Field name='period' type='radio' value={WagePeriod.DailyWage}>
